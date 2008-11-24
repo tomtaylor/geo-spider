3ee9005397a3cfdb911a984743d804a2eac0a105
Tom Taylor <tom@tomtaylor.co.uk>
2008-11-24 20:40:26 +0000

    Moved to the latest version of newgem, rather painfully.

--- lib/geo-spider.rb
+++ lib/geo-spider.rb
@@ -1,6 +1,7 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+
 require 'rubygems'
 require 'hpricot'
 require 'open-uri'
@@ -10,15 +11,16 @@ require 'geo-spider/page'
 require 'geo-spider/site'
 
 module GeoSpider
-  
+
+  VERSION = '0.2.3'
   DEFAULT_USER_AGENT = 'geo-spider (http://github.com/tomtaylor/geo-spider)'
-  
+
   def self.user_agent
     @user_agent || DEFAULT_USER_AGENT
   end
-  
+
   def self.user_agent=(user_agent)
     @user_agent = user_agent
   end
-  
+
 end
